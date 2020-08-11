22f5050938b115a766ce74affc71f5b8114b0f0f
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-11 17:03:43 +0200

    Add event listeners and decorator for binding this

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,26 @@
+// DECORATOR = A METHOD DECORATOR
+// first 2 argurments are target and methodname, we don't use it, therefore the underscore
+function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  // console.log(descriptor);
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjustedDescriptor;
+}
+
+// PROJECTINPUT CLASS
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -14,7 +33,32 @@ class ProjectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      '#people'
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  @AutoBind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+    console.log(this.descriptionInputElement.value);
+    console.log(this.peopleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler);
   }
 
   private attach() {
