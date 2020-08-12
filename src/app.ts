0b869ff2f4b123e1f72589b5e4957b70fe6676e3
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-12 15:52:28 +0200

    Add getter to class

--- src/app.ts
+++ src/app.ts
@@ -163,6 +163,15 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 // PROJECT ITEM CLASS
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   private project: Project;
+
+  get persons() {
+    if (this.project.people === 1) {
+      return '1 person';
+    } else {
+      return `${this.project.people} persons`;
+    }
+  }
+
   constructor(hostId: string, project: Project) {
     super('single-project', hostId, false, project.id);
     this.project = project;
@@ -173,9 +182,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   configure() {}
   renderContent() {
     this.element.querySelector('h2')!.textContent = this.project.title;
-    this.element.querySelector(
-      'h3'
-    )!.textContent = this.project.people.toString();
+    this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
     this.element.querySelector('p')!.textContent = this.project.description;
   }
 }
