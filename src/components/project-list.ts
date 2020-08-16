d1f49ae188145101a88e5784c1431b53bee630f8
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-16 20:12:22 +0200

    Use different import-export syntax

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,4 +1,4 @@
-import { Component } from './base-component.js';
+import Component from './base-component.js';
 import { Project, ProjectStatus } from '../models/project.js';
 import { AutoBind } from '../decorators/autobind.js';
 import { DragTarget } from '../models/drag-drop.js';
